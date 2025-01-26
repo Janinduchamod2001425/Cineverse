75677ce7086367022dc40a27720c1fe4caa8cd9c
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-01-26 12:17:13 +0530

    Complete header with search component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import Search from "./components/Search.jsx";
 
 const App = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src={"./hero.png"} alt="Hero Banner" />
+            <h1>
+              Find <span className="text-gradient ">Movies</span> You'll Enjoy
+              Without the Hassle
             </h1>
+          </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
-    )
-}
-export default App
+      </div>
+    </main>
+  );
+};
+export default App;
