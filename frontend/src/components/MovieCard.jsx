ecbd05777a0777d702ffed7d1a0f93f90d91b5e8
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-01-28 11:38:08 +0530

    Add hover effect to Movie Cards

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -37,48 +37,50 @@ const MovieCard = ({
   const genreNames = genre_ids?.map((id) => genresMap[id]).filter(Boolean);
 
   return (
-    <div className="movie-card">
-      {/*Movie Poster*/}
-      <img
-        src={
-          poster_path
-            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-            : "no-movie.png"
-        }
-        alt={title}
-      />
-      {/*Movie Title*/}
-      <div className="mt-4">
-        <h3>{title}</h3>
+    <div className="movie-card-container">
+      <div className="movie-card">
+        {/*Movie Poster*/}
+        <img
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+              : "no-movie.png"
+          }
+          alt={title}
+        />
+        {/*Movie Title*/}
+        <div className="mt-4">
+          <h3>{title}</h3>
 
-        <div className="content">
-          <div className="rating">
-            <img src="star.svg" alt="Star" />
-            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+          <div className="content">
+            <div className="rating">
+              <img src="star.svg" alt="Star" />
+              <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+            </div>
+
+            <span>•</span>
+            <p className="lang">{original_language}</p>
+
+            <span>•</span>
+            <p className="year">
+              {release_date ? release_date.split("-")[0] : "N/A"}
+            </p>
           </div>
 
-          <span>•</span>
-          <p className="lang">{original_language}</p>
-
-          <span>•</span>
-          <p className="year">
-            {release_date ? release_date.split("-")[0] : "N/A"}
-          </p>
+          {/*Genres*/}
+          {/*<div className="genres flex flex-wrap gap-2 mt-4">*/}
+          {/*  {genreNames && genreNames.length > 0 ? (*/}
+          {/*    genreNames.map((genre, index) => (*/}
+          {/*      <span key={index} className="genre">*/}
+          {/*        {genre}*/}
+          {/*        {index < genreNames.length - 1 && " "}*/}
+          {/*      </span>*/}
+          {/*    ))*/}
+          {/*  ) : (*/}
+          {/*    <span className="genre">N/A</span>*/}
+          {/*  )}*/}
+          {/*</div>*/}
         </div>
-
-        {/*Genres*/}
-        {/*<div className="genres flex flex-wrap gap-2 mt-4">*/}
-        {/*  {genreNames && genreNames.length > 0 ? (*/}
-        {/*    genreNames.map((genre, index) => (*/}
-        {/*      <span key={index} className="genre">*/}
-        {/*        {genre}*/}
-        {/*        {index < genreNames.length - 1 && " "}*/}
-        {/*      </span>*/}
-        {/*    ))*/}
-        {/*  ) : (*/}
-        {/*    <span className="genre">N/A</span>*/}
-        {/*  )}*/}
-        {/*</div>*/}
       </div>
     </div>
   );
