dd6041bbd089fc71067c3f608e057d61dee148df
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-01-28 11:40:07 +0530

    Modify the MovieCard.jsx by resolving the warnings

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 
 // Create a map by adding movie genres to the genre id
-const genresMap = {
-  28: "Action",
-  12: "Adventure",
-  16: "Animation",
-  35: "Comedy",
-  80: "Crime",
-  99: "Documentary",
-  18: "Drama",
-  10751: "Family",
-  14: "Fantasy",
-  36: "History",
-  27: "Horror",
-  10402: "Music",
-  9648: "Mystery",
-  10749: "Romance",
-  878: "Science Fiction",
-  10770: "TV Movie",
-  53: "Thriller",
-  10752: "War",
-  37: "Western",
-};
+// const genresMap = {
+//   28: "Action",
+//   12: "Adventure",
+//   16: "Animation",
+//   35: "Comedy",
+//   80: "Crime",
+//   99: "Documentary",
+//   18: "Drama",
+//   10751: "Family",
+//   14: "Fantasy",
+//   36: "History",
+//   27: "Horror",
+//   10402: "Music",
+//   9648: "Mystery",
+//   10749: "Romance",
+//   878: "Science Fiction",
+//   10770: "TV Movie",
+//   53: "Thriller",
+//   10752: "War",
+//   37: "Western",
+// };
 
 const MovieCard = ({
   movie: {
@@ -30,11 +30,11 @@ const MovieCard = ({
     poster_path,
     release_date,
     original_language,
-    genre_ids,
+    // genre_ids,
   },
 }) => {
   // Map genre ids to genre names
-  const genreNames = genre_ids?.map((id) => genresMap[id]).filter(Boolean);
+  // const genreNames = genre_ids?.map((id) => genresMap[id]).filter(Boolean);
 
   return (
     <div className="movie-card-container">
@@ -54,7 +54,7 @@ const MovieCard = ({
 
           <div className="content">
             <div className="rating">
-              <img src="star.svg" alt="Star" />
+              <img src={"star.svg"} alt="Star" />
               <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
             </div>
 
@@ -66,20 +66,6 @@ const MovieCard = ({
               {release_date ? release_date.split("-")[0] : "N/A"}
             </p>
           </div>
-
-          {/*Genres*/}
-          {/*<div className="genres flex flex-wrap gap-2 mt-4">*/}
-          {/*  {genreNames && genreNames.length > 0 ? (*/}
-          {/*    genreNames.map((genre, index) => (*/}
-          {/*      <span key={index} className="genre">*/}
-          {/*        {genre}*/}
-          {/*        {index < genreNames.length - 1 && " "}*/}
-          {/*      </span>*/}
-          {/*    ))*/}
-          {/*  ) : (*/}
-          {/*    <span className="genre">N/A</span>*/}
-          {/*  )}*/}
-          {/*</div>*/}
         </div>
       </div>
     </div>
